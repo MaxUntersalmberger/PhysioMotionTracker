5a9d3db9ab44350b51227e5d4a9d6f38706a5d86
MertenF06 <mertenflantua@outlook.com>
2026-04-23 13:37:20 +0200

    Poging drie :(. Nu wel gelukt

--- Programma Structuur/ui/tab_main.py
+++
@@ -1,8 +0,0 @@
-class TabHome:
-    def __init__(self, logic_instance):
-        self.logic = logic_instance
-        self.window = logic_instance.window
-
-    def setup(self):
-        """Initialiseert de home tab - momenteel geen specifieke logica nodig"""
-        pass
